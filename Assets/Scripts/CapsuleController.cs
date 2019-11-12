05c77bfce545c6d77323590ea27d69321dc62753
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-11-12 12:26:42 +0100

    added cone (spotlight) cutout shader

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -12,20 +12,6 @@ public class CapsuleController : MonoBehaviour {
     private static readonly int p2 = Shader.PropertyToID("_P2"); 
     private static readonly int r = Shader.PropertyToID("_Radius"); 
 
-    //private void Start() {
-    //    if(!Application.isPlaying) return;
-    //    Debug.Log("SADASDASDSDASD");
-    //    var position = transform.position;
-    //    var point1 = position + transform.up * transform.localScale.y / 2.0f;
-    //    var point2 = position - transform.up * transform.localScale.y / 2.0f;
-    //    var empty = new GameObject();
-    //    var go1 = Instantiate(empty);
-    //    var go2 = Instantiate(empty);
-    //    go1.transform.position = point1;
-    //    go2.transform.position = point2;
-    //    Destroy(empty);
-    //}
-
 
     void Update() {
         if(materials == null || materials.Length == 0) return;
