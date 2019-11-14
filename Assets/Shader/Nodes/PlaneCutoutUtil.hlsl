6742a41b0de3803af551be3103be6008af67d414
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-11-14 14:59:50 +0100

    Added simple edge for plane

--- Assets/Shader/Nodes/PlaneCutoutUtil.hlsl
+++ Assets/Shader/Nodes/PlaneCutoutUtil.hlsl
@@ -6,5 +6,17 @@ void IsInFront_float(float3 PlanePosition, float3 PlaneNormal, float3 Position, 
 	IsInFront = dot(dir, PlaneNormal) > 0;
 }
 
+void GetDistanceToPlane_float(float3 PlanePosition, float3 PlaneNormal, float3 Position, out float Distance) {
+	float3 dir = Position - PlanePosition;
+	int isInFront = dot(dir, PlaneNormal) > 0;
+	int isNotInFront = (isInFront + 1) % 2;
+	float sn = -dot(PlaneNormal, (Position - PlanePosition));
+	float sd = dot(PlaneNormal, PlaneNormal);
+	float sb = sn / sd;
+	float3 closestPointOnPlane = Position + sb * PlaneNormal;
+	float dist = distance(Position, closestPointOnPlane);
+	Distance = -dist * isInFront + dist * isNotInFront;
+}
+
 
 #endif
