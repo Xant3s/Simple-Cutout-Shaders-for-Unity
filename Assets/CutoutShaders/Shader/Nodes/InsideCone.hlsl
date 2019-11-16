f8189508faf00b77eb6b345ad94f742beeef5da7
Samuel Truman <samuel.truman@stud-mail.uni-wuerzburg.de>
2019-11-16 15:28:47 +0100

    Fixed cone not working properly (radians instead of degrees), set all base map colors to fully opaque

--- Assets/CutoutShaders/Shader/Nodes/InsideCone.hlsl
+++ Assets/CutoutShaders/Shader/Nodes/InsideCone.hlsl
@@ -1,22 +1,23 @@
 #ifndef INSIDE_CONE_DEFINED
 #define INSIDE_CONE_DEFINED
 
-void IsInsideCone_float(float3 ConeTip, float3 ConeDir, float ConeHeight, float ConeBaseRadius, float3 Position, out bool Out){
-    float3 coneDist = dot(Position - ConeTip, ConeDir);
-    int outside = coneDist > ConeHeight || coneDist < 0;
+
+void IsInsideCone_float(float3 coneTip, float3 coneDir, float coneHeight, float coneBaseRadius, float3 position, out bool isInside){
+    float3 coneDist = dot(position - coneTip, coneDir);
+    int outside = coneDist > coneHeight || coneDist < 0;
     if(outside) {
-        Out = false;
+        isInside = false;
         return;
     }
 
-    float coneRadius = (coneDist / ConeHeight) * ConeBaseRadius;
-    float orthDistance = length((Position - ConeTip) - coneDist * ConeDir);
-    Out = orthDistance < coneRadius;
+    float coneRadius = (coneDist / coneHeight) * radians(coneBaseRadius);
+    float orthDistance = length((position - coneTip) - coneDist * coneDir);
+    isInside = orthDistance < coneRadius;
 }
 
 void GetDistanceToCone_float(float3 coneTip, float3 coneDir, float coneHeight, float coneBaseRadius, float3 position, out float distanceToCone) {
     float coneDist = dot(position - coneTip, coneDir);
-    float coneRadius = (coneDist / coneHeight) * coneBaseRadius;
+    float coneRadius = (coneDist / coneHeight) * radians(coneBaseRadius);
     float orthDistance = length((position - coneTip) - coneDist * coneDir);
     distanceToCone = orthDistance - coneRadius;
 }
